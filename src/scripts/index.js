cc73f14f433ce0ac6d29918b9caa2c840aef7654
motokikasai <kmomoky@hotmail.com>
2020-03-05 11:14:20 +0100

    Create template

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,17 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
 import "../styles/main.scss";
 import "regenerator-runtime/runtime";
+import "bootstrap/scss/bootstrap.scss";
 // Import any additional modules you want to include below \/
 
 // \/ All of your javascript should go here \/
+
+// const baseURL = "https://api.github.com/";
+
+// document.querySelector("main").innerHTML = "Loading...";
+
+fetch(baseURL)
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {});
